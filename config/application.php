5baad8eb36df2f61c42f69f3a446e635c50cc3be
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-06-30 20:11:16 +0200

    Changed use statements to reflect new location for cache and new class for log

--- config/application.php
+++ config/application.php
@@ -1,7 +1,8 @@
 <?php
 use Origin\Core\Configure;
 use Origin\Core\Plugin;
-use Origin\Utility\Cache;
+use Origin\Cache\Cache;
+use Origin\Log\Log;
 
 /*
  * This will go in your config/server.php file once your app has been developed.
@@ -24,6 +25,16 @@ Cache::config('default', ['engine' => 'File']);
  * here. This is used with hashing and key generation by Security.
  */
 Configure::write('Security.pepper', '-----ORIGIN PHP-----');
+/**
+ * This is the key used to encrypt cookies use Security::generateKey()
+ * or make sure length is 32 bytes.
+ */
+Configure::write('Cookie.key', 'facc85a28e6a22e8ae8ab06c75aa2296');
+
+/**
+ * This is the log configuration
+ */
+Log::config('default', ['engine'=>'File']);
 
 /*
  * Load your plugins here
