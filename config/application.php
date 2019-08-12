374fdcd4963f9e1955662b47a018e48065143d58
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-08-12 13:41:25 +0200

    Added Schema.format configuration

--- config/application.php
+++ config/application.php
@@ -20,9 +20,11 @@ Configure::write('Session.timeout', 3600);
 
 Cache::config('default', ['engine' => 'File']);
 
+
+
 /**
  * Generate a random string such as md5(time()) and place
- * here. This is used with hashing and key generation by Security.
+ * here. This is used by Security:hash 
  */
 Configure::write('Security.pepper', '-----ORIGIN PHP-----');
 /**
@@ -31,10 +33,27 @@ Configure::write('Security.pepper', '-----ORIGIN PHP-----');
  */
 Configure::write('Cookie.key', 'facc85a28e6a22e8ae8ab06c75aa2296');
 
+
 /**
  * This is the log configuration
  */
 Log::config('default', ['engine'=>'File']);
+
+/*
+ * This is the default schema format to be used. This is used
+ * by db commands setup/reset/load etc.
+ */
+Configure::write('Schema.format', 'php');
+
+/**
+ * Cache config for database schema
+ */
+Cache::config('origin_model', [
+    'engine' => 'File',
+    'prefix' => 'origin_model_',
+    'duration' => '+5 minutes', // min 2 minutes
+]);
+
 
 /*
  * Load your plugins here
