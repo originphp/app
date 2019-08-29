8bb6df060346a3e7ad81d4dcb9dd2e983dd164b0
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-08-29 13:19:59 +0200

    Added and removed

---
+++ config/.env.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Environment Variables which will be set in $_ENV.
+ * If you need to use a standard .env then place that in this folder.
+ *
+ * # Do not store this or .env files in repos
+ *
+ * @return array
+ */
+return [
+    'DEBUG' => true,
+    'APP_URL' => 'http://localhost',
+    'DB_HOST' => 'db',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => 'root'
+];
