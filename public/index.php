d4c6897585d8e7a30dbd8482ebfb68602c9dfeaf
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-01 13:49:02 +0200

    Changed front controller

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Front Controller.
  */
@@ -7,23 +8,16 @@ define('START_TIME', microtime(true));
 /**
  * Start the Origin Bootstrap Process.
  */
-require dirname(__DIR__) . DIRECTORY_SEPARATOR .'vendor'. DIRECTORY_SEPARATOR .'originphp'. DIRECTORY_SEPARATOR .'framework'. DIRECTORY_SEPARATOR .'src'. DIRECTORY_SEPARATOR .'bootstrap.php';
+require dirname(__DIR__) . '/vendor/originphp/framework/src/bootstrap.php';
 
 /**
  * Mini router for static content on built in server
  */
-if (php_sapi_name() == 'cli-server') {
+if (php_sapi_name() === 'cli-server') {
     $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     if (file_exists(WEBROOT . $path) and is_file(WEBROOT . $path)) {
         return false;
     }
 }
 
-use Origin\Http\Request;
-use Origin\Http\Response;
-use App\Application;
-
-$request = new Request();
-$response =  new Response();
-$Application = new Application($request, $response);
-$response->send();
+require dirname(__DIR__) . '/vendor/originphp/framework/src/Http/server.php';
