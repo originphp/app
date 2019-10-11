b43259e0355277a851264ec60f84ccf096f42535
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-11 04:06:38 +0200

    Changed definition files to PHP

---
+++ config/locales/en_US.php
@@ -0,0 +1,12 @@
+<?php
+return [
+  'name' => 'English (United States)',
+  'decimals' => '.',
+  'thousands' => ',',
+  'currency' => 'USD',
+  'before' => '$',
+  'after' => NULL,
+  'date' => 'n/j/y',
+  'time' => 'g:i a',
+  'datetime' => 'n/j/y, g:i a',
+];
