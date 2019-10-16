a6ef1bfe96d0998a61c70adb9c2da8e9744285d0
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-16 10:55:47 +0200

    adjusted for decoupled packages

--- config/cache.php
+++ config/cache.php
@@ -8,16 +8,8 @@ use Origin\Cache\Cache;
 
 Cache::config('default', [
     'engine' => 'File',
+    'path' => ROOT . '/tmp/cache',
     'duration' => '+60 minutes', // string or number of seconds e.g. 3600,
     'prefix' => 'cache_',
     'serialize' => true // set to false if you going to cache strings such as output
-]);
-
-/**
- * Cache config for database schema
- */
-Cache::config('origin_model', [
-    'engine' => 'File',
-    'prefix' => 'origin_model_',
-    'duration' => '+5 minutes', // min 2 minutes
 ]);
