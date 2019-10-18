aaeb1162fa99566d00b913be898a9cdf6ce2d071
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-18 07:09:09 +0200

    Fixed coding standards

--- config/locales/en_US.php
+++ config/locales/en_US.php
@@ -1,12 +1,12 @@
 <?php
 return [
-  'name' => 'English (United States)',
-  'decimals' => '.',
-  'thousands' => ',',
-  'currency' => 'USD',
-  'before' => '$',
-  'after' => NULL,
-  'date' => 'n/j/y',
-  'time' => 'g:i a',
-  'datetime' => 'n/j/y, g:i a',
+    'name' => 'English (United States)',
+    'decimals' => '.',
+    'thousands' => ',',
+    'currency' => 'USD',
+    'before' => '$',
+    'after' => null,
+    'date' => 'n/j/y',
+    'time' => 'g:i a',
+    'datetime' => 'n/j/y, g:i a',
 ];
--- config/storage.php
+++ config/storage.php
@@ -1,10 +1,11 @@
 <?php
+use Origin\Storage\Storage;
+
 /**
  * Storage Configuration
  * Engines are Local, Ftp, Sftp
  * @see https://www.originphp.com/docs/storage/
  */
-use Origin\Storage\Storage;
 
 Storage::config('default', [
     'engine' => 'Local',
