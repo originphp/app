c62751bb15cedd670c57b557576c27ef63c3c074
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-11-01 16:31:57 +0100

    Added Framework Cache configuration

--- config/cache.php
+++ config/cache.php
@@ -1,8 +1,9 @@
 <?php
 use Origin\Cache\Cache;
+use Origin\Core\Config;
 
 /**
- * Cache engines include File,Apcu,Redis,Memcached
+ * Cache engines include File, Apcu, Redis, Memcached
  * @see https://www.originphp.com/docs/cache/
  */
 
@@ -12,4 +13,18 @@ Cache::config('default', [
     'duration' => '+60 minutes', // string or number of seconds e.g. 3600,
     'prefix' => 'cache_',
     'serialize' => true // set to false if you going to cache strings such as output
+]);
+
+/**
+ * Caching setup used by framework
+ *
+ * - Model uses this to cache table metadata, if you make changes to table
+ * then  clear this cache.
+ */
+Cache::config('origin', [
+    'engine' => 'File',
+    'path' => CACHE . '/origin',
+    'duration' => Config::read('debug') ? '+2 minutes' : '+24 hours',
+    'prefix' => 'cache_',
+    'serialize' => true
 ]);
