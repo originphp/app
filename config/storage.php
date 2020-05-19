70c27e32c45e1baa0771a1568ae626576659f026
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2020-05-19 16:52:03 +0200

    Added email and s3 default settings

--- config/storage.php
+++ config/storage.php
@@ -1,6 +1,6 @@
 <?php
-
 use Origin\Storage\Engine\LocalEngine;
+use Origin\Storage\Engine\S3Engine;
 
 /**
  * Storage Configuration
@@ -12,5 +12,16 @@ return [
     'default' => [
         'className' => LocalEngine::class,
         'root' => ROOT . '/storage'
+    ],
+    's3' => [
+        'className' => S3Engine::class,
+        'credentials' => [
+            'key' => env('S3_KEY'), // * required
+            'secret' => env('S3_SECRET'), // * required
+        ],
+        'region' => 'us-east-1', // * required
+        'version' => 'latest',
+        'endpoint' => env('S3_ENDPOINT'), // for S3 comptabile protocols
+        'bucket' => env('S3_BUCKET') // * required
     ]
 ];
